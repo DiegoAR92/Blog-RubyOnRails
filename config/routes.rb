c8078d643f529477b6efb0fa4fc15625f5146c36
Diego Alvarez Ruiz <diegoalvarezruiz1@gmail.com>
2018-01-28 20:57:12 +0100

    Actualizacion 0.1
    Navegador terminado.
    Routas definidas.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,19 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
+
+	resources :articles
+=begin
+	get "/articles"
+		post "/articles"
+		delete "/articles"
+		get "/articles:id"
+		get "/articles/new"
+		get "/articles:id/edit"
+		patch "articles/:id"
+		put "/articles:id"
+
+	
+=end
+		
 
   root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
