0e8699c9c07bbeff6340907ed8de263b7e4e88d6
Diego Alvarez Ruiz <diegoalvarezruiz1@gmail.com>
2018-01-29 12:58:13 +0100

    Actualizacion 0.1.1
    Creado metodo Delete.
    Validaciones al introducir articulo.
         - Titulo y cuerpo vacios.
         - Cuerpo inferior a 20.
    Añadido boton eliminar.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,26 +2,41 @@ class ArticlesController < ApplicationController
 	
 	# GET / articles
 	def index
+		#Devuelve todos los registros
 		@article = Article.all
 	end
 	#GET /articles/:id
 	def show
+		#Devuelve un registro por su id
 		@article = Article.find(params[:id])
 	end
 
 	#GET /articles/new
 	def new
+		#Crea un nuevo registro
 		@article = Article.new
 	end
 
 	#POST /articles
 	def create
 		@article = Article.new(titulo: params[:article][:titulo], cuerpo: params[:article][:cuerpo])
-		@article.save
-		redirect_to @article
+		if @article.save
+			redirect_to @article
+		else
+			render :new
+		end
 	end
 
-	
+	#delete /articles/:id
+	def destroy
+		@article = Article.find(params[:id])
+		@article.destroy
+		redirect_to articles_path
+
+	end
+
+	#PUT /articles/:id
 	def update
+		
 	end
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,5 @@
 class Article < ApplicationRecord
+	validates :titulo, presence: true, uniqueness: true
+	validates :cuerpo, presence: true, length: {minimum:20}
+
 end
